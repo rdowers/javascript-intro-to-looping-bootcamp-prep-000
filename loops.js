7c2e449f254a902994e5e60deb5c1cef125bff8a
Rob Dowers <rdowers@gmail.com>
2017-10-27 21:06:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,5 +2,9 @@
 
 function forLoop() {
   for (var i = 0; i < 25; i++) {
-    return "Hi";
+    if i === 1 {
+      return "I am 1 strange loop."
+    } else {
+      return "I am ${i} strange loops."
+    };
   };
